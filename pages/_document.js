b9f071edd09d6bc5d0a66e231fbebf6545d3623d
MISA <ravololonantenainamisa7@gmail.com>
2024-08-18 09:20:34 +0300

    add image open graph to vercel

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta property="og:title" content="Portfolio" />
         <meta property="og:description" content="Développé avec NextJS et TailwindCSS" />
-        <meta property="og:image" content="https://drive.google.com/uc?export=view&id=1dyraWCVZ2G8xkzkYDa0ybHrbBrAaKgQO" />
+        <meta property="og:image" content="https://portfolio-main-khaki-iota.vercel.app/images/Macbook-Air-2011x1165.png" />
         <meta property="og:url" content="https://portfolio-main-khaki-iota.vercel.app/" />
         <meta property="og:type" content="website" />
       </Head>
