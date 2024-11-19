8a35412a55ecb316ec58115b965c418ccf78c639
Misa Ravololonantenaina <55345000+Misa100@users.noreply.github.com>
2024-11-19 08:29:21 +0300

    feat : change description on app banner

--- components/shared/AppBanner.js
+++ components/shared/AppBanner.js
@@ -37,7 +37,7 @@ function AppBanner() {
 					}}
 					className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
 				>
-					An Angular Developer <br />
+					A TypeScript Developer <br />
 					& Design Enthusiast
 				</motion.p>
 				<motion.div
